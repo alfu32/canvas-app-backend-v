3741d1e39195683ef83029914362d1073976b537
alfu32 <ioan.alferaru@gmail.com>
2023-04-17 12:54:17 +0200

    fix(json-decode-array)

--- geometry/entity.v
+++ geometry/entity.v
@@ -1,7 +1,6 @@
 module geometry
 
 import json
-import strings
 
 
 pub struct Entity{
@@ -12,9 +11,9 @@ pub mut:
 }
 pub struct EntityMetadata{
 	id string
-	ent_type string=''
+	ent_type string
 	technology string='javascript'
-	text string=''
+	text string
 	tag string='code'
 }
 
@@ -29,25 +28,14 @@ pub fn from_json(j string) !Entity {
 	e.json=j
 	return e
 }
-pub fn entity_from_json_array(j string) ![]Entity {
-	mut ents:=json.decode([]Entity,j)or{
-		panic(err)
+pub fn entity_from_json_array(json_string string) ![]Entity {
+	if json_string == '' {
+		return []
 	}
-	if ents.len == 0 {
-		return ents
-	} else if ents.len == 1 {
-		ents[0].json=j[1..j.len-1]
-		return ents
-	}else {
-		jstring_array:=j[1..j.len-1].replace('},{"id":"','}###----###{').split('###----###')
-		println(jstring_array)
-		for i,mut ent in ents {
-			ent.json='{"id":"${jstring_array[i]}}'
-		}
-		ents[0].json='${jstring_array[0]}}'
-		ents.last().json='{"id":"${jstring_array.last()}}'
-		return ents
+	entities:=json.decode([]Entity,json_string) or {
+		panic("could not decode ((($json_string)))")
 	}
+	return entities
 }
 pub fn (e Entity) get_box() !Box {
 	return json.decode(Box,e.json)
